af462b3e1fcbca20ba22649bde8def6e08519ad5
Mikey <shpoopdy@gmail.com>
2023-03-14 09:56:19 -0700

    Create ObjectPools.cs

--- TowerD/Assets/Enemy/EnemyMover.cs
+++ TowerD/Assets/Enemy/EnemyMover.cs
@@ -9,7 +9,26 @@ public class EnemyMover : MonoBehaviour
 
     void Start()
     {
+      FindPath();
+      ReturnToStart();
         StartCoroutine(FollowPath());
+    }
+
+    void FindPath()
+    {
+      path.Clear();
+
+      GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+      foreach (GameObject waypoint in waypoints)
+      {
+        path.Add(waypoint.GetComponent<Waypoint>());
+      }
+    }
+
+    void ReturnToStart()
+    {
+      transform.position = path[0].transform.position;
     }
 
     IEnumerator FollowPath()
@@ -31,5 +50,7 @@ public class EnemyMover : MonoBehaviour
         
         //yield return new WaitForSeconds(waitTime);
       }
+
+      Destroy(gameObject);
     }
 }
